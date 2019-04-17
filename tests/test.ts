72ffafa43b0e1708570e56790f05870d96b4b938
Matt Masson <mmasson@microsoft.com>
2019-04-17 15:39:56 -0400

    fix formatting

--- tests/test.ts
+++ tests/test.ts
@@ -111,7 +111,7 @@ class SingleLineTokenComparer extends TokenComparer {
 
         // remove whitespace tokens from grammar result
         let r = grammar.tokenizeLine(query, null);
-        let grammarTokens = normalize ? TokenComparer.NormalizeGrammarTokens(r.tokens): r.tokens;
+        let grammarTokens = normalize ? TokenComparer.NormalizeGrammarTokens(r.tokens) : r.tokens;
 
         super(grammarTokens, pqlex.tokens);
     }
@@ -120,18 +120,18 @@ class SingleLineTokenComparer extends TokenComparer {
 describe("Basic grammar tests", () => {
     it("Grammar loaded", () => {
         expect(grammar).to.exist;
-    }),
+    });
 
-        it("Tokens", () => {
-            let state = null;
-            let lines = QueryMultiline.split("\n");
+    it("Tokens", () => {
+        let state = null;
+        let lines = QueryMultiline.split("\n");
 
-            for (let i = 0; i < lines.length; i++) {
-                let r = grammar.tokenizeLine(lines[i], state);
-                state = r.ruleStack;
-                expect(r.tokens.length).to.be.greaterThan(0);
-            }
-        });
+        for (let i = 0; i < lines.length; i++) {
+            let r = grammar.tokenizeLine(lines[i], state);
+            state = r.ruleStack;
+            expect(r.tokens.length).to.be.greaterThan(0);
+        }
+    });
 });
 
 describe("Compare parser tokens", () => {
@@ -140,45 +140,45 @@ describe("Compare parser tokens", () => {
         const r = new SingleLineTokenComparer(query);
         r.assertTokenCount();
         r.assertTokenOffsets();
-    }),
-        it("Text", () => {
-            const query = "\"string one\" & \"string 2\"";
-            const r = new SingleLineTokenComparer(query);
-            r.assertTokenCount();
-            r.assertTokenOffsets();
-        }),
-        it("Numbers", () => {
-            const query = "1 1.1 5e123 534.1223 2.2e555 -1.3";
-            const r = new SingleLineTokenComparer(query);
-            r.assertTokenCount();
-            r.assertTokenOffsets();
-        }),
-        it("Quoted identifier", () => {
-            const query = "#\"identifier with spaces\"";
-            const r = new SingleLineTokenComparer(query);
-            r.assertTokenCount();
-            r.assertTokenOffsets();
-        }),
-        // TODO: Grammar returns single token (good), but it ends at '.' (bad)
-        xit("Identifier", () => {
-            const query = "Table.FromRecords";
-            const r = new SingleLineTokenComparer(query);
-            r.assertTokenCount();
-            r.assertTokenOffsets();
-        }),
-        it("simple function", () => {
-            const query = "x = () => 1";
-            const r = new SingleLineTokenComparer(query);
-            r.assertTokenCount();
-            r.assertTokenOffsets();
-        }),
-        // TODO: duration is flagged as type, but starting # is ignored
-        xit("duration constructor", () => {
-            const query = "#duration(1,1,1,1)";
-            const r = new SingleLineTokenComparer(query);
-            r.assertTokenCount();
-            r.assertTokenOffsets();
-        })
+    });
+    it("Text", () => {
+        const query = "\"string one\" & \"string 2\"";
+        const r = new SingleLineTokenComparer(query);
+        r.assertTokenCount();
+        r.assertTokenOffsets();
+    });
+    it("Numbers", () => {
+        const query = "1 1.1 5e123 534.1223 2.2e555 -1.3";
+        const r = new SingleLineTokenComparer(query);
+        r.assertTokenCount();
+        r.assertTokenOffsets();
+    });
+    it("Quoted identifier", () => {
+        const query = "#\"identifier with spaces\"";
+        const r = new SingleLineTokenComparer(query);
+        r.assertTokenCount();
+        r.assertTokenOffsets();
+    });
+    // TODO: Grammar returns single token (good), but it ends at '.' (bad)
+    xit("Identifier", () => {
+        const query = "Table.FromRecords";
+        const r = new SingleLineTokenComparer(query);
+        r.assertTokenCount();
+        r.assertTokenOffsets();
+    });
+    it("simple function", () => {
+        const query = "x = () => 1";
+        const r = new SingleLineTokenComparer(query);
+        r.assertTokenCount();
+        r.assertTokenOffsets();
+    });
+    // TODO: duration is flagged as type, but starting # is ignored
+    xit("duration constructor", () => {
+        const query = "#duration(1,1,1,1)";
+        const r = new SingleLineTokenComparer(query);
+        r.assertTokenCount();
+        r.assertTokenOffsets();
+    })
 });
 
 describe("Incremental parsing", () => {
@@ -192,9 +192,9 @@ describe("Incremental parsing", () => {
         for (let i = 0; i < lines.length; i++) {
             let r = grammar.tokenizeLine(lines[i], grammarState);
             grammarState = r.ruleStack;
-            
+
             if (pqState == null) {
-                pqState = Lexer.from(lines[i]);                
+                pqState = Lexer.from(lines[i]);
             } else {
                 pqState = Lexer.appendToDocument(pqState, lines[i]);
             }
@@ -210,5 +210,5 @@ describe("Incremental parsing", () => {
             // TODO: PQ offsets are based on document position rather than line position
             // comparer.assertTokenOffsets();
         }
-    })
+    });
 });
